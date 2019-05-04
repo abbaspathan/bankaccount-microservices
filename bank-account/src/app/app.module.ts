dbfcf1710f07ca796c1dc92a716f93d5c76e206b
Pathan <abbas.pathan@capgemini.com>
2019-05-04 16:50:36 +0530

    angular

--- bank-account/src/app/app.module.ts
+++ bank-account/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ResourceNotFoundComponent } from './notfound.component';
+import { RouterModule } from '@angular/router';
+import { appRouter } from './app.routes';
+import { WelcomeComponent } from './welcome.component';
+import { AccountModule } from './account/account.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ResourceNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AccountModule,
+    RouterModule.forRoot(appRouter)
   ],
   providers: [],
   bootstrap: [AppComponent]
